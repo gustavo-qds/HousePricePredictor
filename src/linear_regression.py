15109dbd89f4f16dea92fa1eff05791bf05a9c14
Gustavo <gustavoguspolla@gmail.com>
2026-09-10 18:13:38 +0100

    simple linear regression

--- src/linear_regression.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
-
-df = pd.read_csv("data/Housing.csv")
-print(df.head())
